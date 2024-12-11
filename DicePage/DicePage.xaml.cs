6d68d044c319c99762e86ef104eef0886b355828
Yukimura Creations <Lauri.kokko@protonmail.com>
2024-12-11 08:52:05 +0200

    DiceImages are back
    Now in folder X,D

--- DicePage/DicePage.xaml.cs
+++ DicePage/DicePage.xaml.cs
@@ -4,40 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DicePage;
-partial class DicePage : ContentPage
+namespace DicePage
 {
-    public DicePage
+    public partial class DicePage : ContentPage
+    {
+        public void onDiceImage_Clicked(object sender, EventArgs e)
         {
-
-                public void onDiceImage_Clicked(object sender, EventArgs e)
-            {
-                Random random = new Random();
-                int randomNr = random.Next(1, 7);
-
-                DiceImage.RotateTo(DiceImage.Rotation + 360);
-
-                switch (randomNr)
-                {
-                    case 1:
-                        DiceImage.Source = "one.png";
-                        break;
-                    case 2:
-                        DiceImage.Source = "two.png";
-                        break;
-                    case 3:
-                        DiceImage.Source = "three.png";
-                        break;
-                    case 4:
-                        DiceImage.Source = "four.png";
-                        break;
-                    case 5:
-                        DiceImage.Source = "five.png";
-                        break;
-                    case 6:
-                        DiceImage.Source = "six.png";
-                        break;
-                    }
-                }
+            Rando
         }
+    }
 }
