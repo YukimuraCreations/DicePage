351016c8de2ee5081178ba99911978aba04622d0
Yukimura Creations <Lauri.kokko@protonmail.com>
2024-12-11 09:08:59 +0200

    Update on DicePage xaml cs
    The code is almost done

--- DicePage/DicePage.xaml.cs
+++ DicePage/DicePage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Resources;
 
 namespace DicePage
 {
@@ -10,7 +11,31 @@ namespace DicePage
     {
         public void onDiceImage_Clicked(object sender, EventArgs e)
         {
-            Rando
+            Random random = new Random();
+            int randomNr = random.Next(1, 7);
+
+            switch (randomNr)
+            {
+                case 1:
+                    DiceImage.Source = "one.png";
+                    break;
+                case 2:
+                    Diceimage.Source = "two.png";
+                    break;
+                case 3:
+                    DiceImage.Source = "three.png";
+                    break;
+                case 4:
+                    DiceImage.Source = "four.png";
+                    break;
+                case 5:
+                    Diceimage.Source = "five.png";
+                    break;
+                case 6:
+                    DiceImage.Source = "six.png";
+                    break;
+
+            }
         }
     }
 }
